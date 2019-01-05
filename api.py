bf4d80658db64b75e6839421e2c8387d98eb9d19
Ben Cromwell <manc39@gmail.com>
2019-01-05 15:48:34 +0000

    Integer conversions, fix token variable

--- api.py
+++ api.py
@@ -7,15 +7,16 @@ load_dotenv()
 app = Flask(__name__)
 
 TOKEN = os.getenv('TOKEN')
-LED_PIN = os.getenv('LED_PIN')
-TIMES_TO_BLINK = os.getenv('TIMES_TO_BLINK')
-SECONDS_TO_BLINK_ON = os.getenv('SECONDS_TO_BLINK_ON')
+LED_PIN = int(os.getenv('LED_PIN'))
+TIMES_TO_BLINK = int(os.getenv('TIMES_TO_BLINK'))
+SECONDS_TO_BLINK_ON = int(os.getenv('SECONDS_TO_BLINK_ON'))
 
 
 @app.route('/warning-light/<token>', methods=['POST'])
 def warning_light(token):
     if request.method == 'POST':
-        if token == os.getenv(TOKEN):
+        if token == TOKEN:
+            # @todo accept, return the response, then fork to run the LEDs
             led.go(LED_PIN, num_times=TIMES_TO_BLINK, seconds_on=SECONDS_TO_BLINK_ON)
             return make_response(('', 202))
         else:
